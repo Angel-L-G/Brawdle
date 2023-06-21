23a94e52bd1af62d97ca8f76750a5e7ac4c6130e
Joelyx <joelyx96@gmail.com>
2023-06-21 14:53:02 +0100

    feat: Project initialized close #5

---
+++ Proyect/Brawle/src/main/java/es/joja/Brawle/BrawleApplication.java
@@ -0,0 +1,13 @@
+package es.joja.Brawle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrawleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrawleApplication.class, args);
+	}
+
+}
