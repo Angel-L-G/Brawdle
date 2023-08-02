35b826cfddd76e7c59c98897e5e8e581a9abb22f
JuanManuelMM <moralesmartinjuanmanuel774@gmail.com>
2023-08-02 14:53:23 +0100

    fix: Fixed that table names were in lowercase instead of being in capital letters #19

---
+++ Proyect/Brawle/src/main/java/es/joja/Brawle/contract/GendersContract.java
@@ -0,0 +1,7 @@
+package es.joja.Brawle.contract;
+
+public class GendersContract {
+	private GendersContract() {}
+	public static final String TABLE_NAME = "GENDERS";
+	public static final String NAME = "NAME";
+}
---
+++ Proyect/Brawle/src/main/java/es/joja/Brawle/contract/RolesContract.java
@@ -0,0 +1,7 @@
+package es.joja.Brawle.contract;
+
+public class RolesContract {
+    private RolesContract() {}
+    public static final String TABLE_NAME = "ROLES";
+    public static final String NAME = "NAME";
+}
