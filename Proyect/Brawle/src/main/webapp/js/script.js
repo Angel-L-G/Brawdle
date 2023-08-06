212e44f2d1d02d404d80c79c5eed3d2641eb5ebc
JuanManuelMM <moralesmartinjuanmanuel774@gmail.com>
2023-08-06 15:37:52 +0100

    feat: Created cards for login and register. Im ccurrently working on pop ups to show log in an register forms #11

---
+++ Proyect/Brawle/src/main/webapp/js/script.js
@@ -0,0 +1,19 @@
+
+    function openLogin() {
+        document.getElementById("loginForm").style.display = "block";
+        document.getElementById("cards").style.display = "none";
+    }
+    
+    function closeLogin() {
+        document.getElementById("loginForm").style.display = "none";
+        document.getElementById("cards").style.display = "block";
+    }
+    function openRegister() {
+        document.getElementById("registerForm").style.display = "block";
+        document.getElementById("cards").style.display = "none";
+    }
+    
+    function closeRegister() {
+        document.getElementById("registerForm").style.display = "none";
+        document.getElementById("cards").style.display = "block";
+    }
