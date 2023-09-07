270977b70d587bcd32194d63477bcb47bbb02527
Angel-L-G <angelchiquitopons@gmail.com>
2023-09-07 17:29:12 +0100

    feat&fix: a lot of various fixes and register and login function on the controller and operative web

--- Proyect/Brawle/src/main/java/es/joja/Brawdle/entity/User.java
+++ Proyect/Brawle/src/main/java/es/joja/Brawdle/entity/User.java
@@ -13,6 +13,16 @@ public class User implements Serializable {
 
     public User() {
     }
+    
+    public User(String nick, String password) {
+    	 this.nick = nick;
+         this.password = password;
+         if(role.isBlank() || role.isEmpty() || role == null) {
+         	this.setRole("User");
+         } else {
+ 			this.setRole(role);
+         }
+    }
 
     public User(int id, String nick, String email, String password, String role) {
         this.id = id;
