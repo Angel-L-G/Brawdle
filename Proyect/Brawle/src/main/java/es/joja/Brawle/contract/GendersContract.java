520915b2939d2168ba6ccd1f3ce64ce772d50b24
Angel-L-G <angelchiquitopons@gmail.com>
2023-09-07 16:31:13 +0100

    fix: remove wrong old files

--- Proyect/Brawle/src/main/java/es/joja/Brawle/contract/GendersContract.java
+++
@@ -1,7 +0,0 @@
-package es.joja.Brawle.contract;
-
-public class GendersContract {
-	private GendersContract() {}
-	public static final String TABLE_NAME = "GENDERS";
-	public static final String NAME = "NAME";
-}
--- Proyect/Brawle/src/main/java/es/joja/Brawle/contract/RolesContract.java
+++
@@ -1,7 +0,0 @@
-package es.joja.Brawle.contract;
-
-public class RolesContract {
-    private RolesContract() {}
-    public static final String TABLE_NAME = "ROLES";
-    public static final String NAME = "NAME";
-}
