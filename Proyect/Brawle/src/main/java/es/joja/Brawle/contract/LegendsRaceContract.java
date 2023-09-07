fca113ec0d2c6820428851961d12ead02e6d33b7
Angel-L-G <angelchiquitopons@gmail.com>
2023-09-07 16:31:13 +0100

    fix: remove wrong old files

--- Proyect/Brawle/src/main/java/es/joja/Brawle/contract/LegendsRaceContract.java
+++
@@ -1,8 +0,0 @@
-package es.joja.Brawle.contract;
-
-public class LegendsRaceContract {
-	private LegendsRaceContract() {}
-	public static final String TABLE_NAME = "LEGENDS_RACE";
-	public static final String LEGEND_ID= "LEGEND_ID";
-	public static final String RACE_NAME= "RACE_NAME";
-}
--- Proyect/Brawle/src/main/java/es/joja/Brawle/contract/LegendsWeaponsContract.java
+++
@@ -1,8 +0,0 @@
-package es.joja.Brawle.contract;
-
-public class LegendsWeaponsContract {
-    private LegendsWeaponsContract() {}
-    public static final String TABLE_NAME = "LEGENDS_WEAPONS";
-    public static final String LEGEND_ID = "LEGEND_ID";
-    public static final String WEAPON_NAME = "WEAPON_NAME";
-}
