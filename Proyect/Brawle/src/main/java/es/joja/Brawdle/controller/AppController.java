e14fdc903a94f6953250d6a36412b3bc0fb0bc8a
Angel-L-G <angelchiquitopons@gmail.com>
2023-09-07 15:31:01 +0100

    feat: Start of the file AppController #21

--- Proyect/Brawle/src/main/java/es/joja/Brawdle/controller/AppController.java
+++ Proyect/Brawle/src/main/java/es/joja/Brawdle/controller/AppController.java
@@ -1,4 +1,34 @@
 package es.joja.Brawdle.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import es.joja.Brawdle.dao.GameDAO;
+import es.joja.Brawdle.dao.LegendDAO;
+import es.joja.Brawdle.dao.UserDAO;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
+
 public class AppController {
+	
+	@Autowired
+	UserDAO userDAO;
+	@Autowired
+	LegendDAO legendDAO;
+	@Autowired
+	GameDAO gameDAO;
+	
+	@RequestMapping("/")
+	public ModelAndView index(
+			//@RequestParam(value = "nombre", required = false) String nom
+			HttpServletRequest request) {
+		ModelAndView modelAndView = new ModelAndView();
+		
+		ServletContext app = request.getServletContext();
+		app.getAttribute()
+		
+		return modelAndView;
+	}
+	
 }
