e722ac3c24c5d6581de49ebd32cc406125fb9156
Angel-L-G <angelchiquitopons@gmail.com>
2023-09-07 16:31:13 +0100

    fix: remove wrong old files

--- Proyect/Brawle/src/main/java/es/joja/Brawle/contract/GameContract.java
+++
@@ -1,8 +0,0 @@
-package es.joja.Brawle.contract;
-
-public class GameContract {
-    private GameContract() {}
-    public static final String TABLE_NAME = "GAMES";
-    public static final String ID = "ID";
-    public static final String LEGEND_ID = "LEGEND_ID";
-}
